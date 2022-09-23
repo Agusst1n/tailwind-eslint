45e07cfc37e1105ffb6e71fd3b55079b2ce89350
agusst1n <agustingodoy951@gmail.com>
2022-09-23 03:51:09 -0300

    plantilla lista

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,12 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="App">
-      <p className="text-sm ...">The quick brown fox ...</p>
-      <p className="text-base ...">The quick brown fox ...</p>
-      <p className="text-lg ...">The quick brown fox ...</p>
-      <p className="text-xl ...">The quick brown fox ...</p>
-      <p className="text-2xl ...">The quick brown fox ...</p>
+      <div className="container h-screen w-screen flex justify-center items-center bg-orange-700">
+        <p>Hi</p>
+      </div>
     </div>
   );
 }
